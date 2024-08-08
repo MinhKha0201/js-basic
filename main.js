22158982dd8a2d096269557882d279a689007184
MinhKha <minhkha@lutech.ltd>
2024-08-08 09:00:11 +0700

    third commit

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
     var usersData = []
     var appSelected = []
     var teamSelected = []
-    var isCheckedAll = false
+
 
     const render = (usersData) => {
         usersData.map((user) => {
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 </button>
               </td>
               <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-               <button id="button-app" user-id="${user.id}" data='${JSON.stringify(user.apps)}'
+               <button id="button-app" user-id="${user.id}"
                 class="middle none center mr-4 rounded-lg bg-green-500 py-3 px-6 font-sans text-xs font-bold text-white shadow-md shadow-green-500/20 transition-all hover:shadow-lg hover:shadow-green-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                 data-ripple-light="true"
                 >
@@ -48,8 +48,7 @@ document.addEventListener("DOMContentLoaded", () => {
         appButtons.forEach((button) => {
             button.addEventListener('click', (event) => {
                 const userId = event.target.getAttribute('user-id');
-                const dataString = event.target.getAttribute('data');
-                appOpenModal(userId, dataString)
+                appOpenModal(userId)
             });
         });
     }
@@ -59,49 +58,6 @@ document.addEventListener("DOMContentLoaded", () => {
             const usersResponse = await fetch(usersApi)
             const usersJson = await usersResponse.json()
             usersData = [...usersJson.data]
-            // usersData.map((user) => {
-            //     body.innerHTML += `<tr class="bg-gray-100 border-b">
-            //   <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">${user.id}</td>
-            //   <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-            //     ${user.name}
-            //   </td>
-            //   <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-            //     <button id="button-team" user-id="${user.id}" team-id="${user.teams.map(team => team.id)}"
-            //     class="middle none center mr-4 rounded-lg bg-blue-500 py-3 px-6 font-sans text-xs font-bold text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-            //     data-ripple-light="true"
-            //     >
-            //     Select Team
-            //     </button>
-            //   </td>
-            //   <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-            //    <button id="button-app" user-id="${user.id}" data='${JSON.stringify(user.apps)}'
-            //     class="middle none center mr-4 rounded-lg bg-green-500 py-3 px-6 font-sans text-xs font-bold text-white shadow-md shadow-green-500/20 transition-all hover:shadow-lg hover:shadow-green-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-            //     data-ripple-light="true"
-            //     >
-            //     ${user.apps.length > 0 ? user.apps.length + " Selected" : "Select App"}
-
-            //     </button>
-            //   </td>
-            // </tr>`
-            // })
-
-            // const teamButtons = document.querySelectorAll("#button-team")
-            // teamButtons.forEach((button) => {
-            //     button.addEventListener('click', (event) => {
-            //         // Lấy giá trị của thuộc tính data-id của nút đã bấm
-            //         const teamId = event.target.getAttribute('team-id');
-            //         const userId = event.target.getAttribute('user-id');
-            //         teamOpenModal(teamId, userId)
-            //     });
-            // });
-            // const appButtons = document.querySelectorAll("#button-app")
-            // appButtons.forEach((button) => {
-            //     button.addEventListener('click', (event) => {
-            //         const userId = event.target.getAttribute('user-id');
-            //         const dataString = event.target.getAttribute('data');
-            //         appOpenModal(userId, dataString)
-            //     });
-            // });
             render(usersData)
 
         } catch (error) {
@@ -122,37 +78,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const DeleteAllButton = document.querySelector("#delete-all")
     const count = document.querySelector("#count")
     const confirm = document.querySelector("#confirm")
+    const searchInput = document.querySelector("#search")
+    const searchForm = document.querySelector("#form")
 
-    confirm.addEventListener('click', (event) => {
-        const data = JSON.parse(event.target.getAttribute("data"))
-        const userId = event.target.getAttribute("user-id")
-        usersData = usersData.map(user => {
-            return user.id === parseInt(userId) ? { ...user, apps: [...data] } : user
-        }
-        )
-        appModalClose()
-        var trs = body.querySelectorAll("tr")
-        trs.forEach(tr => {
-            body.removeChild(tr)
-        })
-        render(usersData)
-    })
 
-    const DeleteAll = () => {
-        isCheckedAll = false
-        const lies = document.querySelectorAll("#app-selected-list>li")
-        lies.forEach(li => {
-            li.remove()
-        })
-        appSelected = []
-        confirm.setAttribute("data", JSON.stringify(appSelected))
-        count.innerText = "None Selected"
-        console.log(appSelected);
-    }
-
-    DeleteAllButton.addEventListener("click", () => {
-        DeleteAll()
-    })
 
 
     const appModalClose = () => {
@@ -163,22 +92,51 @@ document.addEventListener("DOMContentLoaded", () => {
         }, 500);
     }
 
-    const appOpenModal = async (userId, dataString) => {
+    const appOpenModal = async (userId) => {
         appModal.classList.remove('fadeOut');
         appModal.classList.add('fadeIn');
         appModal.style.display = 'flex';
         try {
-            isCheckedAll = false
-            const appsResponse = await fetch(appsApi)
-            const appsData = await appsResponse.json()
-            const data = JSON.parse(dataString)
-            appSelected = [...data]
-
-            confirm.setAttribute("data", JSON.stringify(appSelected))
             confirm.setAttribute("user-id", userId)
+            const appsResponse = await fetch(appsApi)
+            var appsData = await appsResponse.json()
+            const user = usersData.filter(user => user.id === parseInt(userId))
+
+            var searchValue = ""
+            searchInput.addEventListener('change', (event) => {
+                searchValue = event.target.value
+            })
+
+            const appsDataTemp = [...appsData]
+            searchForm.addEventListener("submit", (event) => {
+                event.preventDefault()
+                if (searchValue.trim() !== "") {
+                    appsData = appsData.filter(app => app.name.toLowerCase().includes(searchValue.toLowerCase()))
+                }
+                else {
+                    appsData = [...appsDataTemp]
+                }
+                var lies1 = appsList.querySelectorAll("li")
+                lies1.forEach(li => {
+                    appsList.removeChild(li)
+                })
+                appsData.forEach(app => {
+                    if (ids1.length && ids1.includes(app.app_id)) {
+                        appsList.innerHTML += `<li class="flex gap-2 p-4"><input app-id="${app.app_id}" id="${app.app_id}" checked type="checkbox"
+                    ><label for="${app.app_id}">${app.name}</label></li>`
+                    } else {
+                        appsList.innerHTML += `<li class="flex gap-2 p-4"><input app-id="${app.app_id}" id="${app.app_id}" type="checkbox"
+                    ><label for="${app.app_id}">${app.name}</label></li>`
+                    }
+                })
+            })
+
+
+
+            appSelected = [...user[0].apps]
 
             count.innerText = appSelected.length > 0 ? `${appSelected.length} Selected` : "None Selected"
-            const ids1 = data.map(item => item.app_id);
+            const ids1 = user[0].apps.map(item => item.app_id);
 
             var lies1 = appsList.querySelectorAll("li")
             lies1.forEach(li => {
@@ -186,9 +144,11 @@ document.addEventListener("DOMContentLoaded", () => {
             })
             appsData.forEach(app => {
                 if (ids1.length && ids1.includes(app.app_id)) {
-                    appsList.innerHTML += `<li class="flex gap-2 p-4"><input data-object='${JSON.stringify(app)}' checked type="checkbox" name="" id="${app.app_id}"><label for="${app.app_id}">${app.name}</label></li>`
+                    appsList.innerHTML += `<li class="flex gap-2 p-4"><input app-id="${app.app_id}" id="${app.app_id}" checked type="checkbox"
+                    ><label for="${app.app_id}">${app.name}</label></li>`
                 } else {
-                    appsList.innerHTML += `<li class="flex gap-2 p-4"><input data-object='${JSON.stringify(app)}' type="checkbox" name="" id="${app.app_id}"><label for="${app.app_id}">${app.name}</label></li>`
+                    appsList.innerHTML += `<li class="flex gap-2 p-4"><input app-id="${app.app_id}" id="${app.app_id}" type="checkbox"
+                    ><label for="${app.app_id}">${app.name}</label></li>`
                 }
             })
 
@@ -208,6 +168,32 @@ document.addEventListener("DOMContentLoaded", () => {
             // all checkboxes
             const checkboxes = document.querySelectorAll('li>input[type="checkbox"]');
             const checkedAll = document.querySelector("#all")
+            var isCheckedAll = true
+
+            const DeleteAll = () => {
+                const lies = document.querySelectorAll("#app-selected-list>li")
+                lies.forEach(li => {
+                    li.remove()
+                })
+                appSelected = []
+                checkedAll.checked = false
+                checkboxes.forEach(checkbox => {
+                    checkbox.checked = false
+                })
+                count.innerText = "None Selected"
+            }
+
+            DeleteAllButton.addEventListener("click", () => {
+                DeleteAll()
+            })
+
+            for (let checkbox of checkboxes) {
+                if (checkbox.checked === false) {
+                    isCheckedAll = false
+                    break
+                }
+            }
+            checkedAll.checked = isCheckedAll
             checkedAll.addEventListener("change", e => {
                 isCheckedAll = e.target.checked;
                 if (isCheckedAll) {
@@ -215,16 +201,14 @@ document.addEventListener("DOMContentLoaded", () => {
                         checkbox.checked = true
                     })
                     appSelected = [...appsData]
-                    confirm.setAttribute("data", JSON.stringify(appSelected))
                     count.innerText = `${appSelected.length} Selected`
                     appSelected.map(app => {
                         appsSelectedList.innerHTML += `<li class="${app.app_id} flex justify-between p-4 items-center">
-                    <p class="max-w-[200px] text-wrap">
-                      ${app.name}</p>
-                    <button class="text-red-500">Delete</button>
-                  </li>`
+                                                            <p class="max-w-[200px] text-wrap">
+                                                            ${app.name}</p>
+                                                            <button class="text-red-500">Delete</button>
+                                                        </li>`
                     })
-                    confirm.setAttribute("data", JSON.stringify(appSelected))
                 }
                 else {
                     checkboxes.forEach(checkbox => {
@@ -235,8 +219,6 @@ document.addEventListener("DOMContentLoaded", () => {
                         li.remove()
                     })
                     appSelected = []
-
-                    confirm.setAttribute("data", JSON.stringify(appSelected))
                     count.innerText = appSelected.length > 0 ? `${appSelected.length} Selected` : "None Selected"
                 }
             })
@@ -245,30 +227,54 @@ document.addEventListener("DOMContentLoaded", () => {
             checkboxes.forEach(checkbox => {
                 checkbox.addEventListener('change', (event) => {
                     const isChecked = event.target.checked;
-                    const app = event.target.getAttribute('data-object')
+                    const appId = event.target.getAttribute('app-id')
+                    const app = appsData.filter(app => app.app_id === appId)
                     if (isChecked) {
-                        appSelected.push(JSON.parse(app))
+                        appSelected = [...appSelected, ...app]
                         count.innerText = `${appSelected.length} Selected`
-                        confirm.setAttribute("data", JSON.stringify(appSelected))
-                        appsSelectedList.innerHTML += `<li class="${JSON.parse(app).app_id} flex justify-between p-4 items-center">
+                        appsSelectedList.innerHTML += `<li class="${app[0].app_id} flex justify-between p-4 items-center">
                                                             <p class="max-w-[200px] text-wrap">
-                                                            ${JSON.parse(app).name}</p>
-                                                            <button app-id="${JSON.parse(app).app_id}" class="text-red-500">Delete</button>
+                                                            ${app[0].name}</p>
+                                                            <button app-id="${app[0].app_id}" class="text-red-500">Delete</button>
                                                         </li>`
                     } else {
-                        isCheckedAll = false
-                        appSelected = appSelected.filter(app => app.app_id !== event.target.getAttribute("id"))
-                        confirm.setAttribute("data", JSON.stringify(appSelected))
+                        appSelected = appSelected.filter(app => app.app_id !== appId)
+                        checkedAll.checked = false
                         count.innerText = appSelected.length > 0 ? `${appSelected.length} Selected` : "None Selected"
-                        const li = appsSelectedList.getElementsByClassName(`${JSON.parse(app).app_id}`)
+                        const li = appsSelectedList.getElementsByClassName(`${app[0].app_id}`)
                         console.log(li);
                         appsSelectedList.removeChild(li[0])
                     }
-
-                    console.log(appSelected);
                 });
             });
-
+            confirm.addEventListener('click', (event) => {
+                confirm.innerText = "Loading..."
+                setTimeout(() => {
+                    const userId = event.target.getAttribute("user-id")
+                    usersData = usersData.map(user => {
+                        return user.id === parseInt(userId) ? { ...user, apps: [...appSelected] } : user
+                    }
+                    )
+                    appModalClose()
+                    confirm.innerText = "Confirm"
+                    var trs = body.querySelectorAll("tr")
+                    trs.forEach(tr => {
+                        body.removeChild(tr)
+                    })
+                    render(usersData)
+                }, 1000)
+                // const userId = event.target.getAttribute("user-id")
+                // usersData = usersData.map(user => {
+                //     return user.id === parseInt(userId) ? { ...user, apps: [...appSelected] } : user
+                // }
+                // )
+                // appModalClose()
+                // var trs = body.querySelectorAll("tr")
+                // trs.forEach(tr => {
+                //     body.removeChild(tr)
+                // })
+                // render(usersData)
+            })
 
         } catch (error) {
             console.log(error);
